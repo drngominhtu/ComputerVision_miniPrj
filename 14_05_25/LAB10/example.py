64bf5c48ca0e7d9bad4e33982c06d38b2be20a50
dr.ngominhtu <84098481+drngominhtu@users.noreply.github.com>
2025-05-14 18:18:16 +0700

    lab10[contour]

---
+++ 14_05_25/LAB10/example.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import numpy as np
+
+
+img = cv.imread('image3.png')
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# Áp dụng ngưỡng
+ret, thresh = cv.threshold(gray, 33, 255, cv.THRESH_BINARY)
+
+# Tìm contour
+contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+# Vẽ tất cả contour
+img_contours = img
+cv.drawContours(img_contours, contours, -1, (0, 255, 0), 10)
+
+# Hiển thị kết quả
+cv.imshow('Original', img, )
+cv.imshow('Threshold', thresh)
+cv.imshow('Contours', img_contours)
+cv.waitKey(0)
+cv.destroyAllWindows()
--- 14_05_25/LAB10/main.py
+++ 14_05_25/LAB10/main.py
@@ -1,12 +1,16 @@
 import cv2 as cv
 import numpy as np
 
-im = cv.imread('image.png')
+im = cv.imread('image3.png')
 
 imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
 
-ret, thresh = cv.threshold(imgray, 127, 255, 0)
+ret, thresh = cv.threshold(imgray, 33, 255, cv.THRESH_BINARY)
+
 contour,hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-cv.drawContours(im, contour, -1, (0, 255, 0), 3)
+cv.drawContours(im, contour, -1, (0, 255, 0), 3)  #cv.drawContours(image, contours[outline], contourIdx, color, thickness)
+
 cv.imshow('Contours', im)
+cv.waitKey(0)
+cv.destroyAllWindows()
