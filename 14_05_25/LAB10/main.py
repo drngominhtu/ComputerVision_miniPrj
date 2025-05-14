cfd17db44ff60db4c5575a8a6006a5ed4a043dfa
dr.ngominhtu <84098481+drngominhtu@users.noreply.github.com>
2025-05-14 18:00:49 +0700

    change location

--- 14_05_25/LAB10/main.py
+++ 14_05_25/LAB10/main.py
@@ -7,3 +7,6 @@ imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
 
 ret, thresh = cv.threshold(imgray, 127, 255, 0)
 contour,hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+cv.drawContours(im, contour, -1, (0, 255, 0), 3)
+cv.imshow('Contours', im)
